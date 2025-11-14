8be4d74b78338479721ebdbb7ae95a8977f13897
Legitmod17 <neshkin2018@yandex.ru>
2025-11-14 14:51:49 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "russian");
+
+	cout << "[+] Календарь" << endl;
+	cout << "[1] Январь" << endl;
+	cout << "[2] Февраль" << endl;
+	cout << "[3] Март" << endl;
+	cout << "[4] Апрель" << endl;
+	cout << "[5] Май" << endl;
+	cout << "[6] Июнь" << endl;
+	cout << "[7] Июль" << endl;
+	cout << "[7] Август" << endl;
+}
