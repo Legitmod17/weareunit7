4dea1d14f1be92ba7d88d234ddb82db3e1ea8eea
Legitmod17 <neshkin2018@yandex.ru>
2025-11-20 14:56:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,5 +12,34 @@ int main() {
 	cout << "[5] Май" << endl;
 	cout << "[6] Июнь" << endl;
 	cout << "[7] Июль" << endl;
-	cout << "[7] Август" << endl;
+	cout << "[8] Август" << endl;
+	cout << "[9] Сентябрь" << endl;
+	cout << "[10] Октябрь" << endl;
+	cout << "[11] Ноябрь" << endl;
+	cout << "[12] Декабрь" << endl;
+
+	int month, day;
+	cout << "Выберите месяц: ";
+	cin >> month;
+	cout << endl;
+
+	string monthName;
+
+	switch (month) { 
+	case 1: {
+		cout << "Выбран месяц Январь\n";
+		monthName = "Января";
+		break;
+	}
+	case 2: {
+		cout << "Выбран месяц Февраль\n";
+		monthName = "Февраль";
+		break;
+	}
+	case 3: {
+		cout << "Выбран месяц Март\n";
+		monthName = "Март";
+		break;
+	}
+
 }
