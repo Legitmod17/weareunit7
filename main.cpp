03c493e71e1f56b9f7eb413979aeb952363ce213
Legitmod17 <neshkin2018@yandex.ru>
2025-11-27 14:59:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -94,6 +94,14 @@ int main() {
 		return 0;
 
 
+      
+    }
+
+	cout << "Выберите день: ";
+	cin >> day;
+	switch (day) {
+	case 1: break; case 2: break; case 3: break; case 4: break; case 5: break; case 6: break; case 7: break;
 
 	}
+    
 }
